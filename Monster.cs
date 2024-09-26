319d91934ff2fb0ad26f4c7a40454dcb7d2c95c2
o.o <6whiteowl6@gmail.com>
2024-09-26 16:19:02 +0900

    [feat] Monster 클래스 생성
    Monster 클래스 파일로 분리, main 함수에 몬스터 데이터 추가

---
+++ Monster.cs
@@ -0,0 +1,26 @@
+﻿//using System.Xml;
+//using System.Xml.Linq;
+//using System.Xml.Serialization;
+
+namespace TextRPG_project
+{
+    internal partial class Program
+    {
+        class Monster
+        {
+            public int level;
+            private string name;
+            public int health;
+            public int attack;
+
+            public Monster(string _name, int _level, int _health, int _attack)
+            {
+                name = _name;
+                level = _level;
+                health = _health;
+                attack = _attack;
+            }
+        }
+    }
+
+}
